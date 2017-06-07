f572d780b602f6fab354973e58d7533e6721afd0
Nove <hohoh.422@gmail.com>
2017-06-07 14:23:27 +0800

    利用owlcarousel2插件完成幻灯片广告，实现响应式图片。 利用picturefill兼容IE系列浏览器

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Nove on 2017/6/7.
+ */
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+    });
+});
